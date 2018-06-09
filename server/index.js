d8495629ebfe3b1dd7ade70084f1e46161a700bd
Ubuntu <ubuntu@ip-10-0-0-28.us-west-2.compute.internal>
2018-06-09 00:57:22 +0000

    tweaking session stuff

--- server/index.js
+++ server/index.js
@@ -18,11 +18,9 @@ async function apiAsync(req, res) {
   let args = req.body;
 
   let token = req.header("X-PoundRandom-Auth-Token");
-  console.log("token=", token);
   let userId = null;
   if (token) {
     userId = await data.userForTokenAsync(token);
-    console.log("userId=", userId);
     if (!userId) {
       console.warn("No session for auth token: " + token);
     }
@@ -33,7 +31,7 @@ async function apiAsync(req, res) {
     args = [req.params.a, req.params.b, req.params.c, req.params.d, req.params.e, req.params.f, req.params.g];
   }
   let context = {userId};
-  let callsig = method + JSON.stringify(args).replace(/^./,"(").replace(/.$/,")");
+  let callsig = "[" + (context.userId || "") + "]" + method + JSON.stringify(args).replace(/^./,"(").replace(/.$/,")");
   console.log("API: " + callsig);
   try {
     let result = await api.callMethodAsync(context, method, args);
