74ffc0e5cb132dad597011e2e6b51d0f83035f17
Juwan Wheatley <datwheat@gmail.com>
2018-06-11 15:45:36 -0700

    [ui] get a flatlist rendering something resembling a post

--- pound-random-app/screens/Home.js
+++ pound-random-app/screens/Home.js
@@ -1,255 +1,76 @@
+import React from "react";
 import {
   Button,
   Text,
   TextInput,
   View,
+  TouchableOpacity,
   StyleSheet,
-  FlatList
+  Dimensions,
+  FlatList,
+  ImageBackground
 } from "react-native";
-import React from "react";
+import { LinearGradient } from "expo";
 
-import Api from "../Api";
-import Signup from "../Signup";
+class FeedPost extends React.Component {
+  state = {
+    imageIsLoaded: false
+  };
+
+  randomBit = Math.random();
+
+  render() {
+    return (
+      <TouchableOpacity>
+        <ImageBackground
+          onLoad={() => {
+            this.setState({ imageIsLoaded: true });
+          }}
+          source={{
+            uri: `https://picsum.photos/${
+              Dimensions.get("window").width
+            }/80/?random=${this.randomBit}`
+          }}
+          style={{ width: "100%", height: 80 }}
+        >
+          {this.state.imageIsLoaded ? (
+            <LinearGradient
+              colors={["rgba(0,0,0,0.4)", "transparent"]}
+              style={{
+                flex: 1,
+                position: "absolute",
+                height: "100%",
+                width: "100%"
+              }}
+            />
+          ) : null}
+
+          <View style={{ width: "100%", height: 80, padding: 16 }}>
+            <Text style={{ color: "white" }}>This is a FeedPost</Text>
+          </View>
+        </ImageBackground>
+      </TouchableOpacity>
+    );
+  }
+}
+
+const Separator = () => <View style={{ height: 8 }} />;
 
 export default class Home extends React.Component {
   static navigationOptions = {
     header: null
   };
 
-  state = {
-    username: "",
-    mobileNumber: ""
-  };
-
-  _sortOfSignupAsync = async () => {
-    try {
-      let result = await Api.callMethodAsync("sortOfSignup", {
-        username: this.state.username,
-        mobileNumber: this.state.mobileNumber
-      });
-      console.log(result);
-    } catch (err) {
-      console.error(err);
-    }
-  };
-
   render() {
-    let s = {
-      fontSize: 40
-    };
-
     return (
       <View style={styles.container}>
-        <Text>i'm home</Text>
-
-        {/* <Adder />
-        <ErrorButton />
-        <FakePosts /> */}
-      </View>
-    );
-  }
-}
-
-class FakePosts extends React.Component {
-  state = {
-    loaded: false,
-    data: null
-  };
-
-  _loadDataAsync = async () => {
-    let data = await Api.callMethodAsync("fakePosts");
-    this.setState({ data, loaded: true });
-  };
-
-  componentDidMount() {
-    this._loadDataAsync();
-  }
-
-  render() {
-    if (this.state.loaded) {
-      return (
-        <View
-          style={{
-            marginTop: 40,
-            flex: 1
-          }}
-        >
-          <FlatList
-            data={this.state.data}
-            renderItem={({ item }) => {
-              return (
-                <View
-                  style={{
-                    borderColor: "gray",
-                    borderWidth: 1
-                  }}
-                >
-                  <Text
-                    style={{
-                      fontWeight: "bold"
-                    }}
-                  >
-                    {item.title}
-                  </Text>
-                  <Text>{item.content}</Text>
-                </View>
-              );
-            }}
-          />
-        </View>
-      );
-    } else {
-      return <Text>Loading Fake Posts...</Text>;
-    }
-  }
-}
-
-class Adder extends React.Component {
-  state = {
-    a: "0",
-    b: "0",
-    result: null,
-    loading: false
-  };
-
-  _updateAsync = async () => {
-    this.setState({ loading: true });
-
-    let a = parseInt(this.state.a);
-    let b = parseInt(this.state.b);
-    console.log("Remotely adding ", a, b);
-    let result = await Api.callMethodAsync(
-      "subtract",
-      parseInt(this.state.a),
-      parseInt(this.state.b)
-    );
-    this.setState({ result, loading: false });
-  };
-
-  render() {
-    let s = {
-      fontSize: 60
-    };
-    return (
-      <View>
-        <TextInput
-          keyboardType="numeric"
-          style={s}
-          value={this.state.a}
-          onChangeText={text => {
-            this.setState({ a: text });
-          }}
-          onSubmitEditing={() => {
-            this._updateAsync();
-          }}
-        />
-        <TextInput
-          keyboardType="numeric"
-          style={s}
-          value={this.state.b}
-          onChangeText={text => {
-            this.setState({ b: text });
-          }}
-          onSubmitEditing={() => {
-            this._updateAsync();
-          }}
-        />
-        {(this.state.loading && <Text>Loading...</Text>) || (
-          <Text>{this.state.result}</Text>
-        )}
-      </View>
-    );
-  }
-}
-
-class SimpleAdd extends React.Component {
-  state = {
-    loading: false,
-    result: null
-  };
-
-  _addAsync = async () => {
-    let result = await Api.callMethodAsync("add", 42, 5);
-    this.setState({ result, loading: false });
-  };
-
-  componentDidMount() {
-    this._addAsync();
-  }
-
-  render() {
-    return (
-      <View>
-        <Text>Simple Adder</Text>
-        {(this.state.loading && <Text>Loading...</Text>) || (
-          <Text>{"result=" + this.state.result}</Text>
-        )}
-      </View>
-    );
-  }
-}
-
-class SortOfLogin extends React.Component {
-  state = {
-    username: null
-  };
-
-  _submitAsync = async () => {
-    let result = await Api.callMethodAsync("sortOfLogin", this.state.username);
-    await api.storeSessionAsync(result.token);
-    console.log(result);
-  };
-
-  render() {
-    return (
-      <View
-        style={{
-          marginBottom: 50
-        }}
-      >
-        <Text>Username:</Text>
-        <TextInput
-          autoCapitalize="none"
-          autoCorrect={false}
-          value={this.state.username}
-          onChangeText={text => {
-            this.setState({ username: text });
-          }}
-          onSubmitEditing={() => {
-            this._submitAsync();
-          }}
-        />
-        <Button
-          title="Login"
-          onPress={() => {
-            this._submitAsync();
-          }}
+        <FlatList
+          style={{ flex: 1, width: "100%" }}
+          data={[{ key: "a" }, { key: "b" }, { key: "c" }]}
+          renderItem={({ item }) => <FeedPost />}
+          ItemSeparatorComponent={Separator}
         />
       </View>
-    );
-  }
-}
-
-class ErrorButton extends React.Component {
-  _callErrorApiAsync = async () => {
-    try {
-      let _x = await Api.callMethodAsync("error");
-      console.log("Woah, didn't get an error");
-    } catch (err) {
-      if (err && err.type === "CLIENT_ERROR") {
-        console.log("ClientError:", err, err.code, err.props);
-      }
-    }
-  };
-
-  render() {
-    return (
-      <Button
-        title="Error"
-        onPress={() => {
-          this._callErrorApiAsync();
-        }}
-      />
     );
   }
 }
@@ -257,7 +78,6 @@ class ErrorButton extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
     alignItems: "center",
     backgroundColor: "white"
   },
