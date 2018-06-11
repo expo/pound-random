aa54b1024bda7ae9ede3ace7622c0b4b4063edc9
Juwan Wheatley <datwheat@gmail.com>
2018-06-11 14:47:27 -0700

    [formatting] fix changes due to local comma rules

--- pound-random-app/App.js
+++ pound-random-app/App.js
@@ -7,7 +7,7 @@ import {
   StyleSheet,
   Text,
   TextInput,
-  View,
+  View
 } from "react-native";
 import { createStackNavigator } from "react-navigation";
 
@@ -32,6 +32,6 @@ export default class App extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-  },
+    flex: 1
+  }
 });
