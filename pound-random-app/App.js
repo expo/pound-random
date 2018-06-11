e5f5ef51afbd3be1ec08540d0af5506b8af27c2d
Charlie Cheever <ccheever@gmail.com>
2018-06-11 11:55:16 -0700

    Sort of fixing things

--- pound-random-app/App.js
+++ pound-random-app/App.js
@@ -14,9 +14,6 @@ import { createStackNavigator } from "react-navigation";
 import Login from "./screens/Login";
 import Home from "./screens/Home";
 
-let SERVER_ROOT =
-  "http://ec2-34-219-33-58.us-west-2.compute.amazonaws.com:3200";
-
 const StackNavigator = createStackNavigator(
   { Login, Home },
   { initialRouteName: "Login", headerMode: "none" }
@@ -28,57 +25,9 @@ export default class App extends React.Component {
       <View style={styles.container}>
         <StackNavigator />
         <StatusBar barStyle="dark-content" />
-        <Adder />
-        <Signup />
-        <SortOfLogin />
-        <ErrorButton />
+         <SortOfLogin />
       </View>
-    );
-  }
-}
-
-class SortOfLogin extends React.Component {
-  state = {
-    username: null
-  };
-
-  _submitAsync = async () => {
-    let result = await Api.callMethodAsync("sortOfLogin", this.state.username);
-    await api.storeSessionAsync(result.token);
-    console.log(result);
-  };
-
-  render() {
-    return (
-      <View
-        style={{
-          marginBottom: 50
-        }}
-      >
-        <Text>Username:</Text>
-        <TextInput
-          autoCapitalize="none"
-          autoCorrect={false}
-          value={this.state.username}
-          onChangeText={text => {
-            this.setState({ username: text });
-          }}
-          onSubmitEditing={() => {
-            this._submitAsync();
-          }}
-        />
-        <Button
-          title="Login"
-          onPress={() => {
-            this._submitAsync();
-          }}
-        />
-        <Adder />
-        <Signup />
-        <SortOfLogin />
-        <ErrorButton />
-       </View>
-    );
+    )
   }
 }
 
