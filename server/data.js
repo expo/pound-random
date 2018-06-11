be20ed31656ddd7b270e48f10bc12fe9fc78fc8d
Charlie Cheever <ccheever@gmail.com>
2018-06-11 10:25:57 -0700

    Switching to new database schema
    with column names that lineup with JS object properties more

--- server/data.js
+++ server/data.js
@@ -2,7 +2,7 @@ let db = require("./db")
 let typedError = require("./typedError");
 
 async function getUserByIdAsync(userId) {
-  let result = await db.queryAsync("SELECT user_id, username, password, email, date_created FROM user WHERE user_id = ?", [userId]);
+  let result = await db.queryAsync("SELECT userId, normalizedUsername, displayUsername, email, emailConfirmed, mobileNumber, mobileNumberConfirmed, hashedPassword, createdTime, updatedTime FROM user WHERE userId = ?", [userId]);
   if (result.length < 1) {
     throw new typedError("NOT_FOUND", "No user found for userId " + userId);
   }
@@ -12,13 +12,13 @@ async function getUserByIdAsync(userId) {
 
 async function addUserAsync(user) {
   let u = {...user};
-  u.date_created = u.date_created || Date.now();
-  let result = await db.queryAsync("INSERT INTO user (user_id, username, display_username, mobile_number, password, email, date_created) VALUES (?, ?, ?, ?, ?)", [u.user_id, u.username, u.display_username, u.mobile_number, u.password, u.email, u.date_created]);
-  return u.user_id;
+  u.createdTime = u.createdTime || new Date();
+  let result = await db.queryAsync("INSERT INTO user (userId, noramlizedUsername, displayUsername, email, emailConfirmed, mobileNumber, mobileNumberConfirmed, hashedPassword, createdTime, updatedTime) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", [u.userId, u.normalizedUsername, u.displayUsername,u.email, u.emailConfirmed, u.mobileNumber, u.mobileNumberConfirmed, u.hashedPassword, u.createdTime || new Date(), u.updatedTime || new Date()]);
+  return u.userId;
 }
 
 async function createSessionAsync(userId, token) {
-  let result = await db.queryAsync("INSERT INTO session (user_id, token, created_time) VALUES (?, ?, ?)", [userId, token, Date.now()]);
+  let result = await db.queryAsync("INSERT INTO session (userId, token, createdTime, updatedTime) VALUES (?, ?, ?, ?)", [userId, token, new Date(), new Date());
   return token;
 }
 
@@ -27,14 +27,14 @@ async function deleteSessionAsync(token) {
 }
 
 async function userForTokenAsync(token) {
-  let result = await db.queryAsync("SELECT user_id FROM session WHERE token = ?", [token]);
+  let result = await db.queryAsync("SELECT userId FROM session WHERE token = ?", [token]);
   if (result.length > 0) {
     return result[0].user_id;
   }
 }
 
 async function userIdForUsernameAsync(username) {
-  let result = await db.queryAsync("SELECT user_id FROM user WHERE username = ?", [username]);
+  let result = await db.queryAsync("SELECT userId FROM user WHERE username = ?", [username]);
   if (result.length > 0) {
     return result[0].user_id;
   }
