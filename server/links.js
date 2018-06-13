c28ccdc35637153007180bb8e9291a0dfd03870b
Charlie Cheever <ccheever@gmail.com>
2018-06-13 02:51:07 -0700

    Nicer Clearbit errors

--- server/links.js
+++ server/links.js
@@ -1,10 +1,19 @@
 let clearbit = require("./clearbit");
 
 async function companyInfoForDomainAsync(domain) {
-  let clearbitInfo = await clearbit.Company.find({domain});
-  return {
-    name: clearbitInfo.name,
-    logo: clearbitInfo.logo,
+  try {
+    let clearbitInfo = await clearbit.Company.find({ domain });
+    return {
+      name: clearbitInfo.name,
+      logo: clearbitInfo.logo,
+    }
+  } catch (e) {
+    if (e.name === "CompanyQueuedError") {
+      console.log("Got a Clearbit CompanyQueuedError for " + domain + "... It will probably work next time.");
+    } else {
+      console.warn(e.toString());
+    }
+    return {};
   }
 }
 
