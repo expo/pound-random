49daf827a691ed3408e5603ea2085dca2cc97c55
Charlie Cheever <ccheever@gmail.com>
2018-06-13 02:23:20 -0700

    huh, trying more stuff

--- server/secret.js
+++ server/secret.js
@@ -2,29 +2,30 @@
 if (process.env.RENDER_PRODUCTION) {
   module.exports = require('/etc/secrets/pound-random-secret');
 } else {
-  let placesToTry = [
-    './pound-random-secret',
-    '../../pound-random-secret',
-    '/etc/secrets/pound-random-secret',
-  ];
+  module.exports = {};
+  // let placesToTry = [
+  //   './pound-random-secret',
+  //   '../../pound-random-secret',
+  //   '/etc/secrets/pound-random-secret',
+  // ];
 
-  let ok = false;
+  // let ok = false;
 
-  for (let p of placesToTry) {
-    try {
-      module.exports = require(p);
-      ok = true;
-      break;
-    } catch (e) {
-      if ((e.code === 'MODULE_NOT_FOUND') || (e.toString().startsWith('Error: Cannot find module'))) {
-        continue;
-      } else {
-        throw e;
-      }
-    }
-  }
+  // for (let p of placesToTry) {
+  //   try {
+  //     module.exports = require(p);
+  //     ok = true;
+  //     break;
+  //   } catch (e) {
+  //     if ((e.code === 'MODULE_NOT_FOUND') || (e.toString().startsWith('Error: Cannot find module'))) {
+  //       continue;
+  //     } else {
+  //       throw e;
+  //     }
+  //   }
+  // }
 
-  if (!ok) {
-    throw new Error("Didn't find database configuration. Try cloning https://github.com/expo/pound-random-secret and then symlinking pound-random-secret in the server/ directory to the root of that repo.");
-  }
+  // if (!ok) {
+  //   throw new Error("Didn't find database configuration. Try cloning https://github.com/expo/pound-random-secret and then symlinking pound-random-secret in the server/ directory to the root of that repo.");
+  // }
 }
