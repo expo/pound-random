aa84ded4507be5538fffcc5b8e845e3e15062071
Juwan Wheatley <datwheat@gmail.com>
2018-06-13 16:06:12 -0700

    get image previews working in new post screen

--- pound-random-app/screens/NewPost.js
+++ pound-random-app/screens/NewPost.js
@@ -7,6 +7,7 @@ import {
   TextInput,
   View,
   Dimensions,
+  KeyboardAvoidingView,
   TouchableOpacity
 } from "react-native";
 import Expo from "expo";
@@ -79,7 +80,26 @@ export default class NewPost extends React.Component {
     console.log(result);
 
     if (!result.cancelled) {
-      this.setState({ image: result.uri });
+      Image.getSize(
+        result.uri,
+        (srcWidth, srcHeight) => {
+          const maxHeight = Dimensions.get("window").height; // or something else
+          const maxWidth = Dimensions.get("window").width - 80;
+
+          const ratio = Math.min(maxWidth / srcWidth, maxHeight / srcHeight);
+
+          this.setState({
+            image: {
+              width: srcWidth * ratio,
+              height: srcHeight * ratio,
+              uri: result.uri
+            }
+          });
+        },
+        error => {
+          console.log("error:", error);
+        }
+      );
     }
   };
 
@@ -87,98 +107,126 @@ export default class NewPost extends React.Component {
     const { image } = this.state;
 
     return (
-      <View
+      <KeyboardAvoidingView
+        behavior="padding"
         style={{
           flex: 1,
-          justifyContent: "center",
-          alignItems: "center",
-          backgroundColor: "white"
+          backgroundColor: "transparent"
         }}
       >
-        {image && (
-          <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />
-        )}
-        <TextInput
-          style={{
-            width: Dimensions.get("window").width - 48,
-            backgroundColor: "#C4C4C4",
-            borderRadius: 12,
-            paddingLeft: 16,
-            paddingRight: 16,
-            paddingTop: 16,
-            textAlign: "center",
-            paddingBottom: 16,
-            textAlignVertical: "center",
-            justifyContent: "center",
-            alignItems: "center",
-            fontSize: 18,
-            fontWeight: "600"
-          }}
-          multiline
-          underlineColorAndroid="transparent"
-          autoFocus
-          placeholder="Deep thoughts go here 🧐"
-          onChangeText={text => {
-            this.setState({ text });
-          }}
-        />
-        <TouchableOpacity
-          onPress={() => {
-            this._pickImage();
-          }}
-          style={{
-            justifyContent: "center",
-            alignItems: "center",
-            backgroundColor: "#828282",
-            paddingHorizontal: 32,
-            paddingVertical: 8,
-            marginVertical: 8,
-            borderRadius: 8
-          }}
-        >
-          <Feather name="image" size={16} color="white" />
-        </TouchableOpacity>
-        {this.state.linkInfo && (
-          <View>
-            <Text>{this.state.url}</Text>
-            <Image
-              source={{
-                uri: this.state.linkInfo.domain.logo
-              }}
-              style={{ height: 32, width: 32 }}
-            />
-            <Text>{this.state.linkInfo.domain.name}</Text>
-          </View>
-        )}
-
         <View
           style={{
-            position: "absolute",
-            bottom: 0,
-            paddingVertical: 24,
-            flexDirection: "row",
-            width: "100%",
+            flex: 1,
+            justifyContent: "space-between",
+            alignItems: "center",
+            backgroundColor: "white",
             alignItems: "center"
           }}
         >
-          <TouchableOpacity
-            onPress={() => {
-              this.props.navigation.goBack();
+          <View />
+          <View>
+            <View
+              style={{
+                width: Dimensions.get("window").width - 48,
+                borderRadius: 12,
+                padding: 16,
+                alignItems: "center",
+                backgroundColor: "#C4C4C4"
+              }}
+            >
+              {image && (
+                <Image
+                  source={{ uri: image.uri }}
+                  style={{
+                    height: image.height,
+                    width: image.width,
+                    borderRadius: 16
+                  }}
+                  resizeMode="contain"
+                />
+              )}
+              <TextInput
+                style={{
+                  textAlign: "center",
+                  textAlignVertical: "center",
+                  fontSize: 18,
+                  fontWeight: "600"
+                }}
+                multiline
+                underlineColorAndroid="transparent"
+                autoFocus
+                placeholder="Deep thoughts go here 🧐"
+                onChangeText={text => {
+                  this.setState({ text });
+                }}
+              />
+            </View>
+            <TouchableOpacity
+              onPress={() => {
+                this._pickImage();
+              }}
+              style={{
+                justifyContent: "center",
+                alignItems: "center",
+                backgroundColor: "#828282",
+                paddingHorizontal: 32,
+                paddingVertical: 8,
+                marginVertical: 8,
+                alignSelf: "center",
+                borderRadius: 8
+              }}
+            >
+              <Feather name="image" size={16} color="white" />
+            </TouchableOpacity>
+          </View>
+          {this.state.linkInfo && (
+            <View>
+              <Text>{this.state.url}</Text>
+              <Image
+                source={{
+                  uri: this.state.linkInfo.domain.logo
+                }}
+                style={{ height: 32, width: 32 }}
+              />
+              <Text>{this.state.linkInfo.domain.name}</Text>
+            </View>
+          )}
+
+          <View
+            style={{
+              paddingVertical: 24,
+              flexDirection: "row",
+              width: "100%",
+              alignItems: "center"
             }}
-            style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
           >
-            <Text style={{ fontSize: 24, fontWeight: "600" }}>DISCARD</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
-            onPress={() => {
-              this._submitAsync();
-            }}
-          >
-            <Text style={{ fontSize: 24, fontWeight: "600" }}>POST</Text>
-          </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => {
+                this.props.navigation.goBack();
+              }}
+              style={{
+                flex: 1,
+                justifyContent: "center",
+                alignItems: "center"
+              }}
+            >
+              <Text style={{ fontSize: 24, fontWeight: "600" }}>DISCARD</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={{
+                flex: 1,
+                justifyContent: "center",
+                alignItems: "center"
+              }}
+              onPress={() => {
+                this._submitAsync();
+              }}
+            >
+              <Text style={{ fontSize: 24, fontWeight: "600" }}>POST</Text>
+            </TouchableOpacity>
+          </View>
         </View>
-      </View>
+      </KeyboardAvoidingView>
     );
   }
 }
