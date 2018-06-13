602743d407b03ce1500509b6d8b4717403d212f6
schazers <schazers@gmail.com>
2018-06-13 16:08:28 -0700

    Remove old code, update gitignore

--- server/data.js
+++ server/data.js
@@ -1,33 +1,6 @@
 let db = require("./db")
 let typedError = require("./typedError");
 
-// async function getUserByIdAsync(userId) {
-//   let result = await db.queryAsync("SELECT userId, normalizedUsername, displayUsername, email, emailConfirmed, mobileNumber, mobileNumberConfirmed, hashedPassword, createdTime, updatedTime FROM user WHERE userId = ?", [userId]);
-//   if (result.length < 1) {
-//     throw new typedError("NOT_FOUND", "No user found for userId " + userId);
-//   }
-//   // Make it just a plain JS object
-//   return { ...result[0] };
-// }
-
-// async function addUserAsync(user) {
-//   let u = { ...user };
-//   u.createdTime = u.createdTime || new Date();
-//   let result = await db.queryAsync("INSERT INTO user (userId, normalizedUsername, displayUsername, email, emailConfirmed, mobileNumber, mobileNumberConfirmed, hashedPassword, createdTime, updatedTime) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", [u.userId, u.normalizedUsername, u.displayUsername, u.email, u.emailConfirmed, u.mobileNumber, u.mobileNumberConfirmed, u.hashedPassword, u.createdTime || new Date(), u.updatedTime || new Date()]);
-//   return u.userId;
-// }
-
-
-// async function getObjectAsync(id, table, opts) {
-//   table = table || id.replace(/:.*$/, "");
-//   opts = opts || {};
-//   let column = opts.column || table + "Id";
-//   let results = await db.queryAsync("SELECT * FROM " + table + " WHERE " + column + " = ?", [id]);
-//   if (results.length === 1) {
-//     return Object.assign({}, results[0]);
-//   }
-// }
-
 async function multigetObjectsAsync(idList, table, opts) {
   opts = opts || {};
   let column = opts.column || table + "Id";
