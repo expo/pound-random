8e7c37ccd6a6d9c6e6c0ee58fbf67a50c1a98329
Juwan Wheatley <datwheat@gmail.com>
2018-06-14 11:24:55 -0700

    get post metadata transitioning between feed and thread screens

--- pound-random-app/screens/Thread.js
+++ pound-random-app/screens/Thread.js
@@ -17,6 +17,7 @@ import LinkPost from "../components/FeedPosts/LinkPost";
 import Api from "../Api";
 import { Feather } from "@expo/vector-icons";
 import { Transition } from "react-navigation-fluid-transitions";
+import moment from "moment";
 
 const Separator = () => <View style={{ height: 8 }} />;
 
@@ -45,12 +46,38 @@ export default class Thread extends React.Component {
     return (
       <View style={styles.container}>
         <View style={styles.rootPostContainer}>
+          <TouchableOpacity
+            style={{ margin: 16 }}
+            onPress={() => this.props.navigation.goBack()}
+          >
+            <Feather name="arrow-left" size={24} color="white" />
+          </TouchableOpacity>
           <Transition
             shared={`content-${this.props.navigation.getParam("index", 0)}`}
           >
             <Text style={styles.body}>
               {this.props.navigation.getParam("payload", {}).content}
             </Text>
+          </Transition>
+          <Transition
+            shared={`meta-${this.props.navigation.getParam("index", 0)}`}
+          >
+            <View style={styles.metaContainer}>
+              <Text style={styles.meta}>
+                {moment(
+                  this.props.navigation.getParam("payload", {}).createdTime
+                ).fromNow()}
+              </Text>
+              {this.props.navigation.getParam("payload", {}).userId && (
+                <Text style={styles.meta}>
+                  @{
+                    this.props.navigation
+                      .getParam("payload", {})
+                      .userId.split(":")[1]
+                  }
+                </Text>
+              )}
+            </View>
           </Transition>
         </View>
         <FlatList
@@ -95,7 +122,12 @@ const styles = StyleSheet.create({
     alignItems: "center",
     backgroundColor: "white"
   },
-  rootPostContainer: {},
+  rootPostContainer: {
+    width: "100%",
+    justifyContent: "space-between",
+    height: "30%",
+    backgroundColor: "#C4C4C4"
+  },
   rootPost: {},
   textInput: {
     fontSize: 40
@@ -111,8 +143,20 @@ const styles = StyleSheet.create({
   },
   body: {
     fontSize: 14,
+    padding: 16,
+    fontWeight: "600"
+  },
+  metaContainer: {
+    backgroundColor: "rgba(0,0,0,0.6)",
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between"
+  },
+  meta: {
+    fontSize: 12,
     paddingHorizontal: 16,
-    paddingBottom: 16,
+    paddingVertical: 12,
+    color: "white",
     fontWeight: "600"
   }
 });
